4ac2019664912624d8093d79e0325f6efb1bba3f
William Carreras <37045478+williamGOC@users.noreply.github.com>
2020-05-02 16:15:04 -0300

    Add files via upload

---
+++ OpenGL_Introduction/code_#1/code_1.cpp
@@ -0,0 +1,40 @@
+/* GL01Hello.cpp: Test OpenGL/GLUT c/C++ Setup
+ * To compile with -lfreeglut -lglu32 -lopengl32
+ */  
+	
+#include <GL/glut.h>    // GLUT, include glu.h and gl.h
+
+/* Handler for window-repaint event. Call back when the window first appears and
+   whenever the window needs to be re-painted. */
+void display(){
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	// Set background color to black and opaque
+	glClear(GL_COLOR_BUFFER_BIT);			// Clear the color buffer (background)
+
+	// Draw a red 1x1 squared centred at origin
+	glBegin(GL_QUADS);
+		glColor3f(1.0f, 0.0f, 0.0f);	// Red
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glVertex2f( 0.5f,  0.5f);
+		glVertex2f(-0.5f,  0.5f);
+	glEnd();
+
+
+	glFlush();   //Render now
+		
+}
+
+
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv);				// Initialize GLUT
+	glutCreateWindow("OpenGL Title");   // Create a window with the given title.
+	glutInitWindowSize(320, 320);		// Set the window's initial width & height
+	glutInitWindowPosition(50, 50);     // Position the window's initial top-left corner
+	glutDisplayFunc(display);			// Register display callback handler for window re-paint
+	glutMainLoop();
+	
+	return 0;
+}
+
